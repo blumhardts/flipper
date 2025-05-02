1e16c28c2fa6b31a423231af9722da067e9d56d0
Sam Blumhardt <sam.blumhardt@underdogfantasy.com>
2025-05-02 17:04:32 -0500

    TODO: Figure out what expression hash should look like

---
+++ lib/flipper/ui/actions/expression_gate.rb
@@ -0,0 +1,34 @@
+require 'flipper/ui/action'
+require 'flipper/ui/decorators/feature'
+
+module Flipper
+  module UI
+    module Actions
+      class ExpressionGate < UI::Action
+        include FeatureNameFromRoute
+
+        route %r{\A/features/(?<feature_name>.*)/expression/?\Z}
+
+        def post
+          render_read_only if read_only?
+
+          feature = flipper[feature_name]
+
+          expression = Flipper::Expression.build(params['expression'])
+          feature.enable_expression expression
+
+          redirect_to("/features/#{Flipper::UI::Util.escape feature.key}")
+        end
+
+        def delete
+          render_read_only if read_only?
+
+          feature = flipper[feature_name]
+          feature.disable_expression
+
+          redirect_to("/features/#{Flipper::UI::Util.escape feature.key}")
+        end
+      end
+    end
+  end
+end
